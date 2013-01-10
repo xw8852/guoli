9c1c142cfb1ac78ef1e81138d66311d5fef3524b
mapleskip <mapleskip@gmail.com>
2013-01-10 17:17:13 +0800

    增加标题栏处理类

---
+++ guoli/src/com/guoli/hotel/activity/AddUserActivity.java
@@ -0,0 +1,43 @@
+/**
+ * Project Name:Guoli
+ * File Name:AddUserActivity.java
+ * Package Name:com.guoli.hotel.activity
+ * Date:2013-1-10下午4:34:29
+ * Copyright (c) 2013
+ * Company:苏州海客科技有限公司
+ *
+*/
+
+package com.guoli.hotel.activity;
+
+import com.guoli.hotel.R;
+
+import android.os.Bundle;
+
+/**
+ * ClassName:AddUserActivity <br/>
+ * @Description:    新增入住人页面
+ * Date:     2013-1-10 下午4:34:29 <br/>
+ * @author   maple
+ * @version  
+ * @since    JDK 1.6
+ * @see 	 
+ */
+public class AddUserActivity extends BaseActivity {
+    
+    public AddUserActivity(){
+        mLayoutId = R.layout.add_user;
+    }
+    
+    @Override
+    protected void onCreate(Bundle arg0) {
+        super.onCreate(arg0);
+    }
+
+    @Override
+    protected void findViews() {
+
+    }
+
+}
+
--- guoli/src/com/guoli/hotel/activity/SearchHotelActivity.java
+++ guoli/src/com/guoli/hotel/activity/SearchHotelActivity.java
@@ -3,23 +3,24 @@ package com.guoli.hotel.activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.guoli.hotel.R;
 
-public class SearchHotelActivity extends BaseActivity implements OnClickListener {
+public class SearchHotelActivity extends TitleBarActivity {
     
     public SearchHotelActivity() {
         mLayoutId = R.layout.search_hotel;
+        mRightDrawableId = R.drawable.btn_top_phone;
+        mTitleTextId = R.string.search_hotel_title;
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        initTitleBar();
+        showRightBtn();
     }
 
     @Override
@@ -31,43 +32,21 @@ public class SearchHotelActivity extends BaseActivity implements OnClickListener
 
     @Override
     protected void findViews() {
-        Spinner citySpinner = (Spinner) findViewById(R.id.city_list);
-        Button occupancyBtn = (Button) findViewById(R.id.occupancy_date_btn);
         Button leaveBtn = (Button) findViewById(R.id.leave_date_btn);
+        TextView searchBtn = (TextView) findViewById(R.id.search_btn);
+        Button occupancyBtn = (Button) findViewById(R.id.occupancy_date_btn);
+        Spinner citySpinner = (Spinner) findViewById(R.id.city_list);
         Spinner priceSpinner = (Spinner) findViewById(R.id.price_list);
         Spinner starSpinner = (Spinner) findViewById(R.id.star_list);
         Spinner areaSpinner = (Spinner) findViewById(R.id.area_list);
-        TextView searchBtn = (TextView) findViewById(R.id.search_btn);
         
-        citySpinner.setOnClickListener(this);
         occupancyBtn.setOnClickListener(this);
         leaveBtn.setOnClickListener(this);
-        priceSpinner.setOnClickListener(this);
-        starSpinner.setOnClickListener(this);
-        areaSpinner.setOnClickListener(this);
         searchBtn.setOnClickListener(this);
-    }
-    
-    /**
-     * 
-     * initTitleBar:初始化标题栏. <br/>
-     * @author maple
-     * @since JDK 1.6
-     */
-    private void initTitleBar(){
-        TextView rightBtn = getRightButton();
-        if (rightBtn != null) {
-            rightBtn.setVisibility(View.VISIBLE);
-            rightBtn.setOnClickListener(this);
-            rightBtn.setBackgroundResource(R.drawable.btn_top_phone);
-        }
-        TextView titleView = getTitleView();
-        if (titleView != null) {
-            titleView.setText(R.string.search_hotel_title);
-        }
     }
 
     public void onClick(View v) {
+        super.onClick(v);
         switch (v.getId()) {
         case R.id.city_list:
             //入住城市
@@ -87,10 +66,6 @@ public class SearchHotelActivity extends BaseActivity implements OnClickListener
         case R.id.area_list:
             //区域
             break;
-        case R.id.right_btn:
-            //拨号
-            break;
-
         default:
             break;
         }
