fd33e6e2aa4792f6f068ceccff49f6b14a299191
Josn <xw8852xiaowei@gmail.com>
2013-01-10 17:43:50 +0800

    修改底部bar的样式

--- guoli/src/com/guoli/hotel/widget/BottomTabbar.java
+++ guoli/src/com/guoli/hotel/widget/BottomTabbar.java
@@ -3,6 +3,8 @@ package com.guoli.hotel.widget;
 import android.app.Activity;
 import android.support.v4.app.Fragment;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.LinearLayout;
 
 import com.guoli.hotel.R;
 
@@ -12,7 +14,7 @@ import com.guoli.hotel.R;
  * 
  */
 public class BottomTabbar {
-	View root;
+	LinearLayout root;
 
 	/**
 	 * 
@@ -30,9 +32,19 @@ public class BottomTabbar {
 			throw new IllegalArgumentException(
 					"you's activity (or fragment) must be contain the layout file of bottom_tabbar");
 		if (view.getId() == R.id.bottomTabBar)
-			root = view;
+			root = (LinearLayout)view;
 		else
-			root = view.findViewById(R.id.bottomTabBar);
+			root = (LinearLayout)view.findViewById(R.id.bottomTabBar);
+		for (int i = 0; i < root.getChildCount(); i++) {
+			root.getChildAt(i).setOnClickListener(new OnClickListener() {
 
+				public void onClick(View v) {
+					for (int i = 0; i < root.getChildCount(); i++) {
+						root.getChildAt(i).setSelected(false);
+					}
+					v.setSelected(true);
+				}
+			});
+		}
 	}
 }
