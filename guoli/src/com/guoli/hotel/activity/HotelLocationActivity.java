d73e2e5d0b39b35e35882e067881aa8a40f2d7f2
mapleskip <mapleskip@gmail.com>
2013-01-24 21:09:37 +0800

    提交酒店位置资源文件

---
+++ guoli/src/com/guoli/hotel/activity/HotelLocationActivity.java
@@ -0,0 +1,45 @@
+/**
+ * Project Name:SplashActivity
+ * File Name:HotelLocationActivity.java
+ * Package Name:com.guoli.hotel.activity
+ * Date:2013-1-24下午6:12:28
+ * Copyright (c) 2013
+ * Company:苏州海客科技有限公司
+ *
+*/
+
+package com.guoli.hotel.activity;
+
+import android.os.Bundle;
+
+import com.guoli.hotel.R;
+
+/**
+ * ClassName:HotelLocationActivity <br/>
+ * @Description:
+ * Date:     2013-1-24 下午6:12:28 <br/>
+ * @author   maple
+ * @version  
+ * @since    JDK 1.6
+ * @see 	 
+ */
+public class HotelLocationActivity extends BaseActivity {
+    
+    public HotelLocationActivity(){
+        mLayoutId = R.layout.hotel_location_map;
+        mTitleTextId = R.string.hotel_location_map;
+    }
+    
+    @Override
+    protected void onCreate(Bundle arg0) {
+        super.onCreate(arg0);
+        showLeftBtn();
+    }
+
+    @Override
+    protected void findViews() {
+
+    }
+
+}
+
