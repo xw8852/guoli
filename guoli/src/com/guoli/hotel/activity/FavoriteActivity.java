6f95fa082958c10debdb36879cb3a54a59dbbaeb
mapleskip <mapleskip@gmail.com>
2013-01-27 19:24:07 +0800

    增加我的收藏页面

--- guoli/src/com/guoli/hotel/activity/FavoriteActivity.java
+++ guoli/src/com/guoli/hotel/activity/FavoriteActivity.java
@@ -1,8 +1,8 @@
 /**
  * Project Name:SplashActivity
- * File Name:HotelListActivity.java
+ * File Name:FavoriteActivity.java
  * Package Name:com.guoli.hotel.activity
- * Date:2013-1-26下午2:40:38
+ * Date:2013-1-26下午4:40:45
  * Copyright (c) 2013
  * Company:苏州海客科技有限公司
  *
@@ -13,7 +13,6 @@ package com.guoli.hotel.activity;
 import java.util.ArrayList;
 import java.util.List;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -21,33 +20,31 @@ import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
 
 import com.guoli.hotel.R;
-import com.guoli.hotel.adapter.HotelAdapter;
+import com.guoli.hotel.adapter.FavoriteAdapter;
 import com.guoli.hotel.bean.HotelInfo;
-import com.guoli.hotel.widget.BottomTabbar;
 
 /**
- * ClassName:HotelListActivity <br/>
- * @Description:    特别推荐页面
- * Date:     2013-1-26 下午2:40:38 <br/>
+ * ClassName:FavoriteActivity <br/>
+ * @Description:    我的收藏页面
+ * Date:     2013-1-26 下午4:40:45 <br/>
  * @author   maple
  * @version  
  * @since    JDK 1.6
  * @see 	 
  */
-public class HotelListActivity extends BaseActivity implements OnItemClickListener {
+public class FavoriteActivity extends BaseActivity implements OnItemClickListener {
     
     private ListView mListView;
     
-    public HotelListActivity(){
-        mTitleTextId = R.string.recommend;
-        mLayoutId = R.layout.hotel_recommend_layout;
-        mRightDrawableId = R.drawable.btn_top_phone;
+    public FavoriteActivity(){
+        mLayoutId = R.layout.favorite_layout;
+        mTitleTextId = R.string.my_favorite;
+        mRightTextId = R.string.exit;
     }
     
     @Override
     protected void onCreate(Bundle arg0) {
         super.onCreate(arg0);
-        new BottomTabbar(this, 1);
         showLeftBtn();
         showRightBtn();
         updateListView(getList());
@@ -55,31 +52,15 @@ public class HotelListActivity extends BaseActivity implements OnItemClickListen
 
     @Override
     protected void findViews() {
-        mListView = (ListView) findViewById(R.id.hotelListView);
+        mListView = (ListView) findViewById(R.id.favoriteListView);
         mListView.setOnItemClickListener(this);
     }
-    
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        if (mListView.getAdapter() == null) {
+        if (mListView.getAdapter() == null || !(mListView.getAdapter().getCount() > position)) {
             return;
         }
-        HotelInfo info = (HotelInfo) mListView.getAdapter().getItem(position);
-        if (info == null) {
-            return;
-        }
-        enterHotelDetailActivity();
-    }
-    
-    @Override
-    public void onBackPressed() {
-        showExitDialog();
-    }
-    
-    @Override
-    protected void leftBtnClickEvent() {
-        showExitDialog();
     }
     
     /**
@@ -93,30 +74,17 @@ public class HotelListActivity extends BaseActivity implements OnItemClickListen
         if (list == null || list.size() < 1) {
             return;
         }
-        @SuppressWarnings("unchecked")
-        HotelAdapter<HotelInfo> adapter = (HotelAdapter<HotelInfo>) mListView.getAdapter();
+        FavoriteAdapter adapter = (FavoriteAdapter) mListView.getAdapter();
         if (adapter == null) {
-            adapter = new HotelAdapter<HotelInfo>(list, this);
+            adapter = new FavoriteAdapter(list, this);
             mListView.setAdapter(adapter);
-        } else {
-            adapter.clear();
-            adapter.addMore(list);
+            return;
         }
-    }
-    
-    /**
-     * 
-     * enterHotelDetailActivity:跳转到酒店详细页面. <br/>
-     * @author maple
-     * @since JDK 1.6
-     */
-    private void enterHotelDetailActivity(){
-        Intent intent = new Intent();
-        intent.setClass(this, HotelDetailActivity.class);
-        startActivity(intent);
+        adapter.clear();
+        adapter.addMore(list);
     }
 
-    //模拟的数据
+  //模拟的数据
     private List<HotelInfo> getList(){
         List<HotelInfo> infoList = new ArrayList<HotelInfo>();
         for (int index = 0 ; index < 20 ; index++) {
---
+++ guoli/src/com/guoli/hotel/adapter/FavoriteAdapter.java
@@ -0,0 +1,73 @@
+/**
+ * Project Name:SplashActivity
+ * File Name:FavoriteAdapter.java
+ * Package Name:com.guoli.hotel.adapter
+ * Date:2013-1-26下午4:50:33
+ * Copyright (c) 2013
+ * Company:苏州海客科技有限公司
+ *
+*/
+
+package com.guoli.hotel.adapter;
+
+import java.util.List;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.guoli.hotel.R;
+import com.guoli.hotel.bean.HotelInfo;
+
+/**
+ * ClassName:FavoriteAdapter <br/>
+ * @Description:    我的收藏列表适配器
+ * Date:     2013-1-26 下午4:50:33 <br/>
+ * @author   maple
+ * @version  
+ * @since    JDK 1.6
+ * @see 	 
+ */
+public class FavoriteAdapter extends AbstractAdapter<HotelInfo> {
+
+    public FavoriteAdapter(List<HotelInfo> data, Context context) {
+        super(data, context);
+        
+    }
+
+    @Override
+    public View createView(int position, View convertView, ViewGroup parent, LayoutInflater inflater) {
+        if (data == null || data.size() < 1) {
+            return null;
+        }
+        HotelInfo info = getItem(position);
+        if (info == null) {
+            return null;
+        }
+        ViewHolder holder = null;
+        if (convertView == null) {
+            holder = new ViewHolder();
+            convertView = inflater.inflate(R.layout.item_favorite, null);
+            holder.hotelImgView = (ImageView) convertView.findViewById(R.id.hotel_img);
+            holder.nameView = (TextView) convertView.findViewById(R.id.hotel_name);
+            holder.addressView = (TextView) convertView.findViewById(R.id.hotel_address);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+        //TODO 接口返回图片地址后调用异步下载接口加载图片
+        holder.nameView.setText(info.getName() == null ? "" : info.getName());
+        holder.addressView.setText(info.getAddress() == null ? "" : info.getAddress());
+        return convertView;
+    }
+
+    private class ViewHolder {
+        ImageView hotelImgView;
+        TextView nameView;
+        TextView addressView;
+    }
+}
+
