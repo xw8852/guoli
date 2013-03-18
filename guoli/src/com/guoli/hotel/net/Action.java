53a98b29058d8b9f2bdbf60c2a2529e9615112ce
xiaowei <xw8852@126.com>
2013-03-18 21:30:16 +0800

    添加ACTion
    添加ACTion

--- guoli/src/com/guoli/hotel/net/Action.java
+++ guoli/src/com/guoli/hotel/net/Action.java
@@ -17,5 +17,29 @@ public final class Action {
 		 * 会员订单列表
 		 */
 		public static final String USER_ORDER_LIST="order_userorder";
-	}
+        
+        /** 10. 入住价格清单 */
+        public static final String OrderPriceList="order_ordprice";
+        /** 11. 订单价格计算 */
+        public static final String OrderPriceCount="order_ordsumpri";
+        /** 12. 订单提交 */
+        public static final String OrderSubmit="order_submit";
+
+        /** 15. 订单详情 */
+        public static final String OrderDetail="order_details";
+        /** 16. 取消订单 */
+        public static final String Ordercancel="order_cancel";
+        /** 17. 订单退订 */
+        public static final String OrderUnSubScribe="order_unsubscribe";
+    }
+    /**
+     * 通用
+     * @author John
+     *
+     */
+    public static final class General{
+        /**手机验证码*/
+        public static final String MobileValidate="";
+        
+    }
 }
